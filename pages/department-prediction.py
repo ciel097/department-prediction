e6e8e550da947881561943e1216b3e6d75fdfcd8
unknown <ciel097@gmail.com>
2024-09-27 15:52:40 +0900

    refresh_test1

--- pages/department-prediction.py
+++ pages/department-prediction.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
