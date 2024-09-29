688216a19b5221b514890f66c53d58786fb71e90
unknown <ciel000097@gmail.com>
2024-09-29 16:40:52 +0900

    change req37

--- app.py
+++ app.py
@@ -27,3 +27,6 @@ if st.button("이 달의 민원 업무량 예측"):
 if st.button("AI 챗봇"):
     st.switch_page("pages/chatbot.py")
 
+
+
+
--- pages/chatbot.py
+++ pages/chatbot.py
@@ -3,6 +3,11 @@
 #!pip install langchain
 #!pip install langchain_openai
 #!pip install langchain_community
+
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+import openai
+##################################### add streamlit by Ju 240928
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,19 +18,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score
 from joblib import load
 
+
 import matplotlib.font_manager as fm
 import numpy as np
-
-
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
-from langchain.document_loaders import TextLoader
-from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
-from langchain.vectorstores import FAISS
-import openai 
-from openai import OpenAI
-
 ######## 한글폰트 우리것으로 추가
 def unique(list):
     x = np.array(list)
@@ -53,6 +48,7 @@ st.sidebar.page_link("pages/statics.py", label="민원 처리 정보 안내")
 st.sidebar.page_link("pages/workload-predictions.py", label="이 달의 민원 업무량 예측")
 st.sidebar.page_link("pages/chatbot.py", label="AI 챗봇")
 ###########################################
+
 #complaint = st.text_area("챗봇에게 민원내용을 문의하시면 답변해 드립니다.",height=100)
 # 민원 예시
 
@@ -66,22 +62,16 @@ st.sidebar.page_link("pages/chatbot.py", label="AI 챗봇")
 #####################################
 
 api_key = st.secrets["openai_key"] 
+
 embeddings = OpenAIEmbeddings(openai_api_key=api_key)
 db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
 
-embeddings = OpenAIEmbeddings(openai_api_key=api_key)
-
-embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-
-db = Chroma(persist_directory="./chroma_db", embedding_function=embedding_function)
-
-client = OpenAI(api_key=api_key)
 
 def get_response_from_gpt(query, docs):
     context = "\n\n".join([doc.page_content for doc in docs])
 
     openai.api_key = api_key
-    response = client.chat.completions.create(
+    response = openai.chat.completions.create(
         model="gpt-4o",
         messages=[
             {"role": "user", "content": f"{context}\n\n질문: {query}\n\n답변:"}
@@ -90,14 +80,15 @@ def get_response_from_gpt(query, docs):
     return response.choices[0].message.content
 
 prompt = "서초구 민원 내용을 안내하는 친절한 챗봇 역할 수행:"
-user_input = "사용자 입력"
+
+user_input = st.text_area("챗봇에게 민원내용을 문의하시면 답변해 드립니다.",height=100)
 input = prompt + user_input
-docs = db.similarity_search(user_input)
-if docs:
+
+if (st.button("검색")):
+  docs = db.similarity_search(user_input)
+  if docs:
     response = get_response_from_gpt(input, docs)
-    print("챗봇 응답:", response)
-else:
-    print("관련 정보를 찾을 수 없습니다.")
-
-
+    st.write(response)
+  else:
+    st.write("관련 정보를 찾을 수 없습니다.")
 
